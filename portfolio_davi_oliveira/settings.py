0b61e42e05a785217e4d30d6e25addf93d373ab9
Davi Oliveira <davioliveiraes7@gmail.com>
2025-04-03 23:23:43 -0300

    config: deploy settings

--- portfolio_davi_oliveira/settings.py
+++ portfolio_davi_oliveira/settings.py
@@ -26,9 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv(
-    "SECRET_KEY", "django-insecure-5_nez51*izo_sshfvr$pexe38rpa2#k6v5i7_w!af__e#m8g9a"
-)
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
