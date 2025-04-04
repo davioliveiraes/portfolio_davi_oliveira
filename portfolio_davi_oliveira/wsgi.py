53c8071d1db076189cd3f3791de8f01d21f920e6
Davi Oliveira <davioliveiraes7@gmail.com>
2025-04-04 12:32:20 -0300

    config: Deploy settings

--- portfolio_davi_oliveira/wsgi.py
+++ portfolio_davi_oliveira/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_davi_oliveira.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio_davi_oliveira.settings")
 
 application = get_wsgi_application()
+
+from whitenoise import WhiteNoise
+from django.conf import settings
+
+application = WhiteNoise(application)
+application.add_files(settings.MEDIA_ROOT, prefix="media/")
