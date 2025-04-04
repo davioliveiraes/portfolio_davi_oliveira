bb54abe0c5dcf8f20703b7b0cfec6e501f965795
Davi Oliveira <davioliveiraes7@gmail.com>
2025-04-04 06:36:14 -0300

    config: Deploy settings

--- portfolio_davi_oliveira/settings.py
+++ portfolio_davi_oliveira/settings.py
@@ -139,8 +139,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = os.getenv("MEDIA_URL", "/media/")
+MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, "media"))
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
